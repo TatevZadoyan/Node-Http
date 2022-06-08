acd1f8f71c5e34cb558757e08be54c5ca71079bb
Tatev <tzadoyan@gmail.com>
2022-06-08 12:33:48 +0400

    server with html file

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-//npm install @types/node
 
-http.createServer(function(req, res) {
+http.createServer((req, res) => {
+    if (req.url === '/mainpage') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        fs.createReadStream(path.resolve('./index.html')).pipe(res);
+    } else if (req.url === "/mainpage") {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        fs.createReadStream(path.resolve("./style.css")).pipe(res);
+    } else if (req.url === '/person') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            name: 'anna',
+            age: 24,
+            country: 'argentina'
+        }));
+    } else if (req.url === '/text') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('Hello World hfjhfhjfkfkyfkyflyfyfhlfhflfkyfifliufiu!');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('Data not found');
+    }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World hfjhfhjfkfkyfkyflyfyfhlfhflfkyfifliufiu!');
-
-}).listen(8080);
+}).listen(8081);
